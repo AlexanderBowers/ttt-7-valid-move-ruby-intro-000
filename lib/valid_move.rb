ed592d70aaa03f6612507e19d22b7292770a9254
Alexander Bowers <7319abowers@gmail.com>
2020-09-25 05:12:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(taken, good_range)
 valid = taken && good_range
+return valid
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
