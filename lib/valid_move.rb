27a699816b9c47b69b099bff485e9d77cd4dbb12
Alexander Bowers <7319abowers@gmail.com>
2020-09-25 04:51:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(input)
-  valid input = 
+  valid input =
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
