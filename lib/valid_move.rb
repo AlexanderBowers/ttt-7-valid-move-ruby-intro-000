8f6d0dc2423d1996ce3c91f03aacdcd9e260da29
Alexander Bowers <7319abowers@gmail.com>
2020-09-25 04:56:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move(input)
-  valid input =
+def valid_move?(input)
+  valid =
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
@@ -8,3 +8,11 @@ def position_taken?(board, input)
   taken == (board[input] == "X" || board[input] == "O")
   return taken
 end
+
+def valid_range?(input)
+  if (input < 0  || input > 8)
+    valid = false
+  else
+    valid = true
+    return valid
+  
